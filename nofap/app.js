025a56b7a0b3de38f11d74cbc101396db03c7e8e
Lakshay <Lakshaykamat2048@gmail.com>
2022-09-26 17:09:30 +0530

    some new features

--- nofap/app.js
+++ nofap/app.js
@@ -5,6 +5,25 @@ const secEl = document.getElementById('seconds')
 const achieveEl = document.getElementById('achieve')
 const sideBar =  document.getElementById('sidebar')
 const achievementName = document.getElementById('achieveName')
+const quotesEl = document.getElementById('quote')
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '04f144a486mshf8408ceb53634b3p1ac06cjsnfb9e4811bf3f',
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+	}
+};
+
+fetch('https://quotes15.p.rapidapi.com/quotes/random/?language_code=en', options)
+	.then(response => response.json())
+	.then(response => {
+        setQuotes(response.content)
+    })
+	.catch(err => console.error(err));
+
+
+
 const sidebar = ()=>{
     sideBar.style.display = "flex"
 }
@@ -34,43 +53,66 @@ function start(){
     achievement(textDay)
 }
 const achievement = (day)=>{
-    if(day == 0){
-        achieveEl.textContent = "New Bee 🐝"
-        achievementName.children[1].style.color = 'Green'
-    }else if(day == 1){
-        achieveEl.textContent = "Scout 🔫"
-        achievementName.children[1].style.color = 'Green'
-        achievementName.children[2].style.color = 'Green'
-        }else if(day == 3){
-            achieveEl.textContent = "An Intern"   
-            achievementName.children[1].style.color = 'Green'
-            achievementName.children[2].style.color = 'Green' 
-            achievementName.children[3].style.color = 'Green' 
-        }else if(day == 5){
-            achieveEl.textContent = "Corporal"
-            achievementName.children[1].style.color = 'Green'
-            achievementName.children[2].style.color = 'Green'
-            achievementName.children[3].style.color = 'Green'
-            achievementName.children[4].style.color = 'Green'
-        }else if(day == 7){
-            achieveEl.textContent = "Sergeant"
-            achievementName.children[1].style.color = 'Green'
-            achievementName.children[2].style.color = 'Green'
-            achievementName.children[3].style.color = 'Green'
-            achievementName.children[4].style.color = 'Green'
-            achievementName.children[5].style.color = 'Green'
-        }else if(day == 10){
+    if(day >=0 && day < 3){
+        achieveEl.textContent = "New Bee"
+        setStyle(1)
+    }else if(day >=3 && day < 5){
+        achieveEl.textContent = "Private"
+        setStyle(2)
+        }else if(day >= 5 && day < 7){
+            setStyle(3)
+        }else if(day >=7 && day < 10){
             achieveEl.textContent = "Master Sergeant"
-            achievementName.children[1].style.color = 'Green'
-            achievementName.children[2].style.color = 'Green'
-            achievementName.children[3].style.color = 'Green'
-            achievementName.children[4].style.color = 'Green'
-            achievementName.children[5].style.color = 'Green'
-            achievementName.children[6].style.color = 'Green'
+            setStyle(4)
+        }else if(day >= 10 && day < 14){
+            achieveEl.textContent = "Knight"
+            setStyle(5)
+        }else if(day >=14 && day < 21){
+            achieveEl.textContent = "Knight-Lieutenant"
+            setStyle(6)
+        }else if(day >=21 && day < 30){
+            achieveEl.textContent = "Knight-Captain"
+            setStyle(7)
+        }else if(day >=30 && day < 60){
+            achieveEl.textContent = "Knight-Champion"
+            setStyle(8)
+        }else if(day >=60 && day < 90){
+            achieveEl.textContent = "Champion of the Light"
+            setStyle(9)
+        }else if(day >=90 && day < 120){
+            achieveEl.textContent = "Commander"
+            setStyle(10)
+        }else if(day >=120 && day < 150){
+            achieveEl.textContent = "Conqueror"
+            setStyle(11)
+        }else if(day >=150 && day < 180){
+            achieveEl.textContent = "Marshal"
+            setStyle(12)
+        }else if(day >= 180 && day < 240){
+            achieveEl.textContent = "Field Marshal"
+            setStyle(13)
+        }else if(day >= 240 && day < 300){
+            achieveEl.textContent = "Grand Marshal"
+            setStyle(14)
+        }else if(day >= 300 && day < 365){
+            achieveEl.textContent = "High Overload"
+            setStyle(15)
+        }else if(day >= 300 && day < 365){
+            achieveEl.textContent = "High Overload"
+            setStyle(16)
+        }else if(day >= 365){
+            achieveEl.textContent = "Immortal"
+            setStyle(17)
         }
-        else{
-         achieveEl.textContent = "Thinking to add achievement here. Recommend me some keywords. Code are ready!"    
-     }
+}
+function setQuotes(quote){
+    quotesEl.textContent = quote
+}
+function setStyle(i){
+    for(var j = 1;j<=i;j++){
+        achievementName.children[j].style.outline = "1px solid #00203f"
+    }
+
 }
 setInterval(start,1000)
 function popup(){
